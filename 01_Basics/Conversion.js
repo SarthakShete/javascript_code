b6bb616191a1df8860227a27e2dfc1d01e7a26a4
SarthakShete <161003332+SarthakShete@users.noreply.github.com>
2025-10-23 18:10:20 +0000

    Conversion of Datatypes

---
+++ 01_Basics/Conversion.js
@@ -0,0 +1,19 @@
+
+let value = "33ab";
+
+console.log(typeof value);
+
+// convert into the Number dataType...
+// The ans store NaN mean Not Number...
+let ans = Number(value);
+console.log(ans);
+
+
+let x = 1;
+
+let y = Boolean(x);
+console.log(y);
+
+// 1 => true , 0 => false
+// "" => false.
+// "XYZ" => true.
---
+++ 01_Basics/variable.js
@@ -0,0 +1,8 @@
+
+const userId = 1;
+
+let username = "Rajjjj";
+var password = "1234";
+userCity = "Pune"
+
+console.table([userId,username,password,userCity]);
