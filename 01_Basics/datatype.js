de302817bde60f335edcad4ac56cf2c6f400a8dd
SarthakShete <161003332+SarthakShete@users.noreply.github.com>
2025-10-24 08:24:59 +0000

    Dataypes

--- 01_Basics/datatype.js
+++ 01_Basics/datatype.js
@@ -1,2 +1,29 @@
 "use strict"; // treat all js code as newer version...
 
+// primitive 
+
+let num = 3;
+console.log(typeof num);
+
+let bigI = 23412313n;
+console.log(typeof bigI);
+
+let NullValue = null;
+console.log(typeof NullValue);
+
+// Non-Primitive -> object, Array , function
+
+let objectNum = {
+    name : "Raj",
+    age : 23
+};
+
+console.log("The Type of Object is : ",typeof objectNum);
+
+function hello(){
+    console.log("Hello World....");
+}
+console.log("The Type of Function is : ",typeof hello);
+
+let arr  = [3,5,2,5,3];
+console.log("The Type of Array is : ",typeof arr);
